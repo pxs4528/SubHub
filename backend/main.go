ef900b8947f6c914cbe22742fc448fb1401a7547
Dhrutik <solankidhrutik0602@gmail.com>
2023-09-06 22:20:23 -0500

    added-server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "backend/controllers"
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+
 
 func main() {
-	controllers.Server()
+	// starting the server on localhost 8080
+	err := http.ListenAndServe(":8080",nil)
+
+	if err != nil{
+		log.Fatal(err.Error())
+		os.Exit(1)
+	}
+
 }
+
+

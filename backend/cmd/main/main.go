27bb05110e6d5a91b5603af7267e25079e4fbc71
Dhrutik <solankidhrutik0602@gmail.com>
2023-09-12 09:34:32 -0500

    added-conn-pool&starting-server

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"backend/internal/database"
+	"backend/internal/routes"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 
 
 
 func main() {
-	database.Connect()
+	pool := database.Connect()
+
+	router := routes.NewRouter(pool)
+
+	servErr := http.ListenAndServe("localhost:8080",router)
+
+	if errors.Is(servErr,http.ErrServerClosed) {
+		fmt.Fprintf(os.Stderr, "Server Closed: %v\n", servErr)
+		os.Exit(1)
+	} else if (servErr != nil) {
+		fmt.Fprintf(os.Stderr,"Error Starting Server: %v",servErr)
+		os.Exit(1)
+	}
 }

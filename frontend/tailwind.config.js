58bfc6785d58657fe2de78383991ccc27beeeaa4
Parth Sharma <pxs4528@mavs.uta.edu>
2023-09-22 01:08:46 -0500

    LOOOL

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif', 'system-ui']
+    }
+  },
+  plugins: []
+};

16bf445c987011e36fcae4e40fe584632e37f3ca
Parth Sharma <pxs4528@mavs.uta.edu>
2023-09-22 05:08:02 -0500

    forgot to add main lol

---
+++ frontend/main/index.js
@@ -0,0 +1,77 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Native
+const path_1 = require("path");
+// Packages
+const electron_1 = require("electron");
+const electron_is_dev_1 = __importDefault(require("electron-is-dev"));
+const height = 600;
+const width = 800;
+function createWindow() {
+    // Create the browser window.
+    const window = new electron_1.BrowserWindow({
+        width,
+        height,
+        //  change to false to use AppBar
+        frame: false,
+        show: true,
+        resizable: true,
+        fullscreenable: true,
+        webPreferences: {
+            preload: (0, path_1.join)(__dirname, 'preload.js')
+        }
+    });
+    const port = process.env.PORT || 3000;
+    const url = electron_is_dev_1.default ? `http://localhost:${port}` : (0, path_1.join)(__dirname, '../src/out/index.html');
+    // and load the index.html of the app.
+    if (electron_is_dev_1.default) {
+        window?.loadURL(url);
+    }
+    else {
+        window?.loadFile(url);
+    }
+    // Open the DevTools.
+    // window.webContents.openDevTools();
+    // For AppBar
+    electron_1.ipcMain.on('minimize', () => {
+        // eslint-disable-next-line no-unused-expressions
+        window.isMinimized() ? window.restore() : window.minimize();
+        // or alternatively: win.isVisible() ? win.hide() : win.show()
+    });
+    electron_1.ipcMain.on('maximize', () => {
+        // eslint-disable-next-line no-unused-expressions
+        window.isMaximized() ? window.restore() : window.maximize();
+    });
+    electron_1.ipcMain.on('close', () => {
+        window.close();
+    });
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+electron_1.app.whenReady().then(() => {
+    createWindow();
+    electron_1.app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (electron_1.BrowserWindow.getAllWindows().length === 0)
+            createWindow();
+    });
+});
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+electron_1.app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+        electron_1.app.quit();
+});
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+// listen the channel `message` and resend the received message to the renderer process
+electron_1.ipcMain.on('message', (event, message) => {
+    console.log(message);
+    setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
+});
---
+++ frontend/main/preload.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+const api = {
+    /**
+     * Here you can expose functions to the renderer process
+     * so they can interact with the main (electron) side
+     * without security problems.
+     *
+     * The function below can accessed using `window.Main.sayHello`
+     */
+    sendMessage: (message) => {
+        electron_1.ipcRenderer.send('message', message);
+    },
+    /**
+      Here function for AppBar
+     */
+    Minimize: () => {
+        electron_1.ipcRenderer.send('minimize');
+    },
+    Maximize: () => {
+        electron_1.ipcRenderer.send('maximize');
+    },
+    Close: () => {
+        electron_1.ipcRenderer.send('close');
+    },
+    /**
+     * Provide an easier way to listen to events
+     */
+    on: (channel, callback) => {
+        electron_1.ipcRenderer.on(channel, (_, data) => callback(data));
+    }
+};
+electron_1.contextBridge.exposeInMainWorld('Main', api);
+/**
+ * Using the ipcRenderer directly in the browser through the contextBridge ist not really secure.
+ * I advise using the Main/api way !!
+ */
+electron_1.contextBridge.exposeInMainWorld('ipcRenderer', electron_1.ipcRenderer);

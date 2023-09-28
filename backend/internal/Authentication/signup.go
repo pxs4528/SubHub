b92aabe297bb03702d8fd78c24e2649d7ccac4b7
Dhrutik <solankidhrutik0602@gmail.com>
2023-09-28 13:09:35 -0500

    added go routine for signup

--- backend/internal/Authentication/signup.go
+++ backend/internal/Authentication/signup.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -22,9 +23,11 @@ func NewSignUp(response http.ResponseWriter,request *http.Request, pool *pgxpool
 		response.Write([]byte("Mission Json Data"))
 		return
 	}
+	ch := make(chan bool)
+	go UserExist(user,pool,ch)
 
 	user.ID = uuid.New().String()
-	hpass,err := hashPassword(user.Password)
+	hpass,err := HashPassword(user.Password)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		response.Write([]byte("Error hashing password"))
@@ -32,21 +35,31 @@ func NewSignUp(response http.ResponseWriter,request *http.Request, pool *pgxpool
 	}
 	user.Password = string(hpass)
 	user.AuthType = "Regular"
-	
-	_,err = pool.Exec(context.Background(), `INSERT INTO public.users 
-											VALUES ($1,$2,$3,$4,$5);`,user.ID,user.Name,user.Email,user.Password,user.AuthType)
-	if err != nil {
+
+	userExist := <- ch
+
+	if !userExist {
 		response.WriteHeader(http.StatusConflict)
 		response.Write([]byte("User Already Exist"))
 		return
-	}
+	} else {
+		_,_ = pool.Exec(context.Background(), `INSERT INTO public.users 
+												VALUES ($1,$2,$3,$4,$5);`,user.ID,user.Name,user.Email,user.Password,user.AuthType)
 
-	response.WriteHeader(http.StatusCreated)
+		response.WriteHeader(http.StatusCreated)
+	}
 
 }
 
-func hashPassword(password string) ([]byte,error){
-	hpass,err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+func UserExist(user UserData, pool *pgxpool.Pool, ch chan bool){
+	var exists int
+	err := pool.QueryRow(context.Background(), `SELECT * 
+												FROM public.users
+												WHERE email = $1;`,user.Email).Scan(&exists)
+	ch <- err == pgx.ErrNoRows
+}
 
+func HashPassword(password string) ([]byte,error){
+	hpass,err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
 	return hpass,err
 }

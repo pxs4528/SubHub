5f575280f0ddd2c052e48998f7000638d3af6acf
Parth <parthsharma.cs@gmail.com>
2023-11-02 05:14:11 -0500

    PWA implemented, native applications are back bitches

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,19 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata, Viewport } from "next";
+import react from '@/public/icon512_rounded.png'
+
+
+export const metadata : Metadata = {
+  title: "SubHub",
+  description: "Subscriptions made easy",
+  manifest: "/manifest.json",
+  icons: { apple : '/icon-192x192.png.png'}
+};
+
+export const viewport : Viewport = {
+  themeColor: "#ffffff",
+};
 
 export default function RootLayout({
   children,

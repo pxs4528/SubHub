f4f255279c2fbafb70fb851c1b33b8711c2b6714
Dhrutik <solankidhrutik0602@gmail.com>
2023-11-03 23:34:41 -0500

    added JWT verification

--- backend/internal/Authentication/JWT.go
+++ backend/internal/Authentication/JWT.go
@@ -1,8 +1,10 @@
 package authentication
 
 import (
-
+	"backend/internal/Response"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -26,39 +28,85 @@ func (uh *UserHandler) GenerateJWT() (string,error){
 	return tokenString,nil
 }
 
-/*
-func JWT(headerToken string) (string,int,error){
 
-	token,err := jwt.ParseWithClaims(headerToken,&Claims{}, func(t *jwt.Token) (interface{}, error) {
+
+
+func (uh *UserHandler) ValidateJWT(response http.ResponseWriter,request *http.Request) {
+
+	headerToken,headerError := GetJWTHeader(request)
+	if headerError != "" {
+		Response.Send(response,http.StatusUnauthorized,"User Not Authorized",nil)
+		return
+	}
+
+	token,err := jwt.ParseWithClaims(headerToken,&JWT{}, func(t *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("Secret")),nil
 	})
 	if err != nil {
-		return "",http.StatusUnauthorized,err
+		Response.Send(response,http.StatusUnauthorized,"JWT Token Parsing Error",nil)
+		return
 	}
 
 	if !token.Valid {
-		return "",http.StatusUnauthorized,nil
+		Response.Send(response,http.StatusUnauthorized,"Unvalid User Token",nil)
+		return
 	}
 
-	claims,ok := token.Claims.(*Claims)
+	claims,ok := token.Claims.(*JWT)
 	if !ok {
-		return "",http.StatusUnauthorized,nil
+		Response.Send(response,http.StatusUnauthorized,"Error getting claims from JWT",nil)
+		return
 	}
 
 	expTime, err := token.Claims.GetExpirationTime()
 	if err != nil {
-		return "",http.StatusInternalServerError,err
+		Response.Send(response,http.StatusInternalServerError,"Error getting JWT expiration",nil)
+		return
 	}
 
 	if expTime.Unix() < time.Now().Unix() {
-		return "",http.StatusUnauthorized,nil
+		Response.Send(response,http.StatusUnauthorized,"JWT Token Expired",nil)
+		return
 	}
 
-	return claims.ID,http.StatusAccepted,nil
-}
-*/
+	id,headerError := GetAccess(response)
+	if headerError != "" {
+		Response.Send(response,http.StatusUnauthorized,"User not authorized",nil)
+		return
+	}
 
+	if id != claims.ID {
+		Response.Send(response,http.StatusUnauthorized,"ID doesn't match",nil)
+		return
+	}
 
-func (uh *UserHandler) ValidateJWT() {
-	
+	Response.Send(response,http.StatusOK,"User Validated",nil)
+
 }
+
+func GetJWTHeader(request *http.Request) (string,string){
+	if authHeaderValue := request.Header.Get("Authorization"); authHeaderValue != "" {
+		if strings.HasPrefix(authHeaderValue, "Bearer ") {
+			// Remove the "Bearer " prefix
+			token := authHeaderValue[len("Bearer "):]	
+			return token,""
+		}
+	}
+	return "","User not authorized"
+}
+
+func GetAccess(response http.ResponseWriter) (string,string) {
+	if accessToken :=  response.Header().Get("Access"); accessToken != "" {
+		token := accessToken[:]
+		return token,""
+	}
+	return "","User not authorized"
+}
+
+// func GetAccess(request *http.Request) (string,string) {
+// 	if accessToken := request.Header.Get("Access"); accessToken != "" {
+// 		token := accessToken[:]
+// 		return token, ""
+// 	}
+// 	return "","User not authorized"
+// }

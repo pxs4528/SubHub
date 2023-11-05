72a2a191974d05c4fd7a23378fe0041aa682bce3
Dhrutik <solankidhrutik0602@gmail.com>
2023-11-04 22:57:38 -0500

    added subscription

--- frontend/app/lib/data.ts
+++ frontend/app/lib/data.ts
@@ -20,7 +20,8 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    // customer 
+    const data = await sql<Revenue>`SELECT * FROM revenue;`;
 
     // console.log('Data fetch complete after 3 seconds.');
 
--- frontend/app/lib/placeholder-data.js
+++ frontend/app/lib/placeholder-data.js
@@ -14,7 +14,7 @@ const customers = [
     id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
     name: "Delba de Oliveira",
     email: "delba@oliveira.com",
-    image_url: "/customers/delba-de-oliveira.png",
+    image_url: ``,
   },
   {
     id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",

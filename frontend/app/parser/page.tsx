ff723eccc30d2fccbf4790150fc73a62545bc6b9
Parth <parthsharma.cs@gmail.com>
2023-11-09 02:23:58 -0600

    pdf parser terminal working

---
+++ frontend/app/parser/page.tsx
@@ -0,0 +1,11 @@
+import FileUpload from '@/app/components/file-upload';
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='w-96'>
+        <FileUpload />
+      </div>
+    </main>
+  );
+}
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
 const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['pdf2json'],
+    },
     ...withPWA({
         dest: 'public',
         register: true,

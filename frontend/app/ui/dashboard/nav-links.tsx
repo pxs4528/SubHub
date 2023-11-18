f1d6a69808b88e4a94a4341f972e7d3ec1697c2f
Parth <pxs4528@mavs.uta.edu>
2023-11-17 20:03:33 -0600

    lol

--- frontend/app/ui/dashboard/nav-links.tsx
+++ frontend/app/ui/dashboard/nav-links.tsx
@@ -16,7 +16,7 @@ const links = [
     href: "/dashboard/invoices",
     icon: DocumentDuplicateIcon,
   },
-  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+  { name: "Import Data", href: "/parser", icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {

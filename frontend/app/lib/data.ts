164db59821ac9cfd81e815173ca4911240edb974
Parth <pxs4528@mavs.uta.edu>
2023-11-18 04:13:49 -0600

    IT REMEMBERS :sunglasses:

--- frontend/app/lib/data.ts
+++ frontend/app/lib/data.ts
@@ -177,6 +177,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore();
   try {
     const data = await sql<CustomerField>`
       SELECT

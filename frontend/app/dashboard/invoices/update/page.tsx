0e3e9e54c9240525a96c3c9a37eb54e83a83b343
Nick Finestead <naf3595@mavs.uta.edu>
2023-11-25 21:40:53 -0600

    Fixed typing issue in edit-form, need to add fetch request

--- frontend/app/dashboard/invoices/update/page.tsx
+++ frontend/app/dashboard/invoices/update/page.tsx
@@ -3,12 +3,12 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers } from "@/app/lib/data";
 import React, { useEffect, useState } from 'react';
-import { SubscriptionsField } from "@/app/lib/definitions";
+import { CustomerField, SubscriptionsField, SubscriptionsTable } from "@/app/lib/definitions";
 
 
 
 export default function Page() {
-  const [allSubscriptions, setAllSubscriptions] = useState<SubscriptionsField[] | null>(null);
+  const [allSubscriptions, setAllSubscriptions] = useState<SubscriptionsTable[] | null>(null);
   useEffect(() => {
     const getAllsubscriptionData = async () => {
       try {
@@ -28,11 +28,11 @@ export default function Page() {
         console.error("Error fetching subscription data: ", err);
       }
     };
-    getAllsubscriptionData();
+    getAllsubscriptionData(); // Need to work on update component in UI to fix error's but seems to be working decently right now
   }, []);
   return (
     <main>
-      <Form customers={allSubscriptions} />
+      <Form subscriptions={allSubscriptions} /> 
     </main>
   );
 }
--- frontend/app/ui/invoices/edit-form.tsx
+++ frontend/app/ui/invoices/edit-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
+import { CustomerField, InvoiceForm, SubscriptionsField, SubscriptionsTable } from "@/app/lib/definitions";
 import { updateInvoice } from "@/app/lib/actions";
 import {
   CheckIcon,
@@ -11,35 +11,67 @@ import {
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
 
+import { useState } from "react";
+import { UUID } from "crypto";
+
+
+
 export default function EditInvoiceForm({
-  invoice,
-  customers,
+  subscriptions
 }: {
-  invoice: InvoiceForm;
-  customers: CustomerField[];
+  subscriptions: SubscriptionsTable[] | null;
 }) {
+
+  const [subscriptionName, setSubscriptionName] = useState("");
+  const [subscriptionId, setSubscriptionId] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [status, setStatus] = useState("");
+
+  const handleSubscriptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const selectedValue = e.target.value;
+      console.log(selectedValue)
+      const selectedCustomer = subscriptions?.find((customer) => customer.name === selectedValue);
+      if(selectedCustomer)
+      {
+        setSubscriptionName(selectedCustomer.name);
+        setSubscriptionId(selectedCustomer.subscription_id);
+      }
+      else
+        console.log("BOXO")
+    }
+
+    const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setAmount(parseFloat(e.target.value));
+    }
+    const handleStatusChange = (status: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setStatus(status);
+    };
+    console.log(subscriptions)
+
+    // TODO: Handle Form submit, will add maybe tomorrow, or whenever, will need to change action on 53 to be this instead of the braces
   return (
-    <form action={updateInvoice}>
+    <form action={e => {console.log("submitted update :D"); console.log(subscriptionId, subscriptionName, amount, status)}}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Invoice ID */}
-        <input type="hidden" name="id" value={invoice.id} />
+        {/* <input type="hidden" name="id" value={invoice.id} /> */}
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
-            Choose customer
+            Choose subscription
           </label>
           <div className="relative">
             <select
               id="customer"
               name="customerId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={invoice.customer_id}
+              defaultValue=""
+              onChange={handleSubscriptionChange}
             >
               <option value="" disabled>
                 Select a customer
               </option>
-              {customers.map((customer) => (
-                <option key={customer.id} value={customer.id}>
+              {subscriptions?.map((customer) => (
+                <option key={customer.subscription_id} value={customer.subscription_id}>
                   {customer.name}
                 </option>
               ))}
@@ -59,9 +91,10 @@ export default function EditInvoiceForm({
                 id="amount"
                 name="amount"
                 type="number"
-                defaultValue={invoice.amount}
+                step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                onChange={handleAmountChange}
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -81,8 +114,8 @@ export default function EditInvoiceForm({
                   name="status"
                   type="radio"
                   value="pending"
-                  defaultChecked={invoice.status === "pending"}
                   className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  onChange={handleStatusChange("pending")}
                 />
                 <label
                   htmlFor="pending"
@@ -97,8 +130,8 @@ export default function EditInvoiceForm({
                   name="status"
                   type="radio"
                   value="paid"
-                  defaultChecked={invoice.status === "paid"}
                   className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  onChange={handleStatusChange("paid")}
                 />
                 <label
                   htmlFor="paid"

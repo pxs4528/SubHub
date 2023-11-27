c56d68d7e7464ef91de70e56f655cf3e62e639a2
Parth <pxs4528@mavs.uta.edu>
2023-11-26 18:07:10 -0600

    Refactor UI styles in dashboard components

--- frontend/app/dashboard/(overview)/page.tsx
+++ frontend/app/dashboard/(overview)/page.tsx
@@ -104,7 +104,7 @@ export default async function Page() {
   }, [200]);
   return (
     <main>
-      <h1 className={`${lusitana.className} dark:invert mb-4 text-xl md:text-2xl`}>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">

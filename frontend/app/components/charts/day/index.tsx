11143099765282ca8c8db72c9f7d8d2da4badfaa
Dhrutik <solankidhrutik0602@gmail.com>
2023-11-29 01:31:22 -0600

    added dark mode and light mode to manage subscription

--- frontend/app/components/charts/day/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function DayPage() {
-    return <div>Hello Day</div>
-  }
-  
--- frontend/app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Lexend } from "next/font/google";
-
-export const inter = Inter({ subsets: ["latin"] });
-
-export const lusitana = Lexend({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-});
